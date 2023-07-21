8f0c1ea75b3358f37de7dfd75acd672d44ab46e3
Azat <azat.farmanyan2@mail.ru>
2023-07-21 22:39:22 +0400

    feat: create modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'UnaDesk';
+  showModal = false;
 }
