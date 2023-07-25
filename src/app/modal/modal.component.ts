9cea88358edc680e9d7d298ed94e83d81a923d5c
Azat Farmanyan <azat.farmanyan2@mail.ru>
2023-07-25 19:04:57 +0400

    some changes

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,13 +1,25 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   @Output() onClose = new EventEmitter();
   @Output() onUpdate = new EventEmitter();
 
   showRoles = false;
+  form: FormGroup;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      todo: new FormControl('', [Validators.required]),
+    });
+  }
+  update() {
+    console.log('update');
+  }
 }
